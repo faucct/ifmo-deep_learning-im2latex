26ded46921e4af613215da4470544d476d013c85
Nikita Sokolov <faucct@gmail.com>
2019-12-24 11:44:33 +0300

    Fix AttentionCell shapes

--- src/model.py
+++ src/model.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 def images_seq(image):
     return keras.Sequential([
+        keras.layers.Lambda(keras.backend.cast, arguments=dict(dtype='float32'), output_shape=lambda x: x),
         keras.layers.Conv2D(64, 3, strides=1, padding='SAME', activation='relu'),
         keras.layers.MaxPooling2D(2, 2, 'SAME'),
         keras.layers.Conv2D(128, 3, strides=1, padding='SAME', activation='relu'),
@@ -19,24 +20,24 @@ def images_seq(image):
         keras.layers.Conv2D(512, 3, strides=1, padding='VALID', activation='relu'),
         keras_transformer.position.AddPositionalEncoding(),
         keras.layers.Reshape((-1, 512)),
-    ])(keras.backend.cast(image, 'float32'))
+    ])(image)
 
 
 class AttentionCell(keras.layers.Layer):
     def __init__(self, cell, seq, vocabulary_size, **kwargs):
         self.cell = cell
-        self.state_size = list(cell.state_size)
+        self.state_size = list(cell.state_size) + [512]
         self.seq = seq
         self.W1_e = keras.layers.Dense(512, use_bias=False)(seq)
         self.W2_h = keras.layers.Dense(512, use_bias=False)
         self.beta = keras.layers.Dense(1, use_bias=False)
         self.W3_o = keras.layers.Dense(512, use_bias=False, activation='tanh')
         self.W4 = keras.layers.Dense(vocabulary_size, use_bias=False)
-        self.vocabulary_size = vocabulary_size
+        self.output_size = vocabulary_size
         super(AttentionCell, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        self.cell.build(input_shape)
+        self.cell.build((input_shape[0], input_shape[1] + 512))
 
     def call(self, inputs, attention_state):
         """
@@ -44,7 +45,7 @@ class AttentionCell(keras.layers.Layer):
             inputs: shape = (batch_size, dim_embeddings) embeddings from previous time step
             attention_state: (AttentionState) state from previous time step
         """
-        state, o = attention_state
+        *state, o = attention_state
         # compute h
         h, new_state = self.cell.call(keras.layers.Concatenate(axis=-1)([inputs, o]), state)
         # apply previous logic
@@ -61,7 +62,7 @@ class AttentionCell(keras.layers.Layer):
         ]))
         new_o = self.W3_o(keras.layers.Concatenate(axis=-1)([h, c]))
         logits = self.W4(new_o)
-        return logits, [new_state, new_o]
+        return logits, [*new_state, new_o]
 
 
 class TrainingSequence(keras.utils.Sequence):
